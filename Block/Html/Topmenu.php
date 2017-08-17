a771802db138117f4589ca91ba94bcc2220fdf66
Irene <irene.iaccio@gmail.com>
2017-08-17 16:55:27 +0200

    Init modulo

---
+++ Block/Html/Topmenu.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Module Musetti Megamenu
+ *
+ */
+namespace Bitbull\MusettiMenu\Block\Html;
+
+use Magento\Framework\Data\Tree\Node;
+use Magento\Framework\DataObject;
+use Magento\Framework\View\Element\Template;
+
+class Topmenu extends \Magento\Theme\Block\Html\Topmenu
+{
+    /**
+     * Recursively generates top menu html from data that is specified in $menuTree
+     *
+     * @param Node   $menuTree          menu tree
+     * @param string $childrenWrapClass children wrap class
+     * @param int    $limit             limit
+     * @param array  $colBrakes         column brakes
+     * @return string
+     *
+     * @SuppressWarnings(PHPMD)
+     */
+    protected function _getHtml(
+        Node $menuTree,
+        $childrenWrapClass,
+        $limit,
+        $colBrakes = []
+    ) {
+        $html = parent::_getHtml($menuTree, $childrenWrapClass, $limit, $colBrakes = []);
+
+        $transportObject = new DataObject(['html' => $html, 'menu_tree' => $menuTree]);
+        $this->_eventManager->dispatch(
+            'vendor_topmenu_node_gethtml_after',
+            ['menu' => $this->_menu, 'transport' => $transportObject]
+        );
+
+        $html = $transportObject->getHtml();
+
+        return $html;
+    }
+}
---
+++ Observer/AddToCategoryTopmenu.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Topmenu catalog observer to add custom additional elements
+ *
+ */
+namespace Bitbull\MusettiMenu\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Catalog\Api\CategoryRepositoryInterface;
+
+/**
+ * Class AddFirstCategoryImageToTopmenu
+ * @package Vendor\NavigationMenu
+ */
+
+class AddToCategoryTopmenu implements ObserverInterface
+{
+    /**
+     * @var CategoryRepositoryInterface $categoryRepository
+     */
+    protected $categoryRepository;
+
+    /**
+     * AddFirstCategoryImageToTopmenu constructor.
+     *
+     * @param CategoryRepositoryInterface $categoryRepository repository
+     */
+    public function __construct(
+        CategoryRepositoryInterface $categoryRepository
+    ) {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    /**
+     * @param Observer $observer Observer object
+     */
+    public function execute(Observer $observer)
+    {
+        $transport = $observer->getTransport();
+        $html      = $transport->getHtml();
+        $menuTree  = $transport->getMenuTree();
+
+        $parentLevel = $menuTree->getLevel();
+        $childLevel = $parentLevel === null ? 0 : $parentLevel + 1;
+
+        $menuId = $menuTree->getId();
+
+        if ($childLevel == 1 && $this->isCategory($menuId)) {
+            $html .= '<li class="category_image" style=""><img src="'.$this->getCategoryImage($menuId).'"/></li>';
+        }
+
+        $transport->setHtml($html);
+    }
+
+    /**
+     * Retrieves the category image for the corresponding child
+     *
+     * @param string $categoryId Category composed ID
+     *
+     * @return string
+     */
+    protected function getCategoryImage($categoryId)
+    {
+        $categoryIdElements = explode('-', $categoryId);
+        $category           = $this->categoryRepository->get(end($categoryIdElements));
+        $categoryName       = $category->getImageUrl();
+
+        return $categoryName;
+    }
+
+    /**
+     * Check if current menu element corresponds to a category
+     *
+     * @param string $menuId Menu element composed ID
+     *
+     * @return string
+     */
+    protected function isCategory($menuId)
+    {
+        $menuId = explode('-', $menuId);
+
+        return 'category' == array_shift($menuId);
+    }
+}
